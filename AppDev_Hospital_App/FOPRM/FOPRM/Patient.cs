c05ac1ff5e5c56e2de4d9fd9e504254cd2a23515
Quang Vinh Mai <2152339@edu.vaniercollege.qc.ca>
2022-12-06 11:18:03 -0500

    sdfsdf

--- AppDev_Hospital_App/FOPRM/FOPRM/Patient.cs
+++ AppDev_Hospital_App/FOPRM/FOPRM/Patient.cs
@@ -13,8 +13,6 @@ namespace FOPRM
 {
     public class Patient
     {
-
-        //--------------------------
         private string patientId;
         private string fname;
         private string lname;
@@ -48,30 +46,5 @@ namespace FOPRM
         public string MedInsurance { get { return this.medInsurance; } set { this.medInsurance = value; } }
         public string Passport { get { return this.passport; } set { this.passport= value; } }
         public List<string> Diseases { get { return this.diseases; } set { this.diseases = value; } }
-
-        /*
-         * Merge 2 profiles
-         * 
-         * @param p1 first profile
-         * @param p2 second profile
-         * 
-         * p1 becomes p2
-         * p1 diseases appends p2 diseases
-        */
-        public static void merge(Patient p1, Patient p2)
-        {
-            p1.Fname = p2.Fname;
-            p1.Lname = p2.Lname;
-            p1.Age = p2.Age;
-            p1.Gender = p2.Gender;
-            p1.Condition = p2.Condition;
-
-            /*foreach (string e in p2.Diseases)
-            {
-                p1.Diseases.Append(e);
-            }
-            */
-            p1.Diseases = p2.Diseases;
-        }
     }
 }
